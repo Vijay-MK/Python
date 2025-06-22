c41b9a7e6e2b74547cb502f75a10ff685284ebaf
Vijay M K <vijaymathur43@outlook.com>
2025-06-22 14:31:10 +0530

    Commiting the String related python file to the Expression folder

---
+++ Expression/String Concatenation and Repetation.py
@@ -0,0 +1,21 @@
+#String Concatenation and String Repetation
+#only + and * will work with the string datatype
+s ='abc'
+s1 ='def'
+print("The Concatenated string of Sand S1 is :", s+s1)
+
+# if I want to add a integer type data to the string I can str()--> Type Conversion
+
+s2= 'abc'
+s3 = 10
+
+print(s2 + str(s3))
+
+# For string the Multiplication * is the repetation of the same character or string
+
+#for example if I want to print 'a' for 5 times
+print('a'*5)
+b= 'a'*3
+print("b is :",b)
+
+# Sting repetation is only possible with the int type
