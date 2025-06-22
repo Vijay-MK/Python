b886d7c88d7122c75d6068bad7c44d1612ef66a0
Vijay M K <vijaymathur43@outlook.com>
2025-06-22 16:03:16 +0530

    committing the float practices.

---
+++ Expression/float.py
@@ -0,0 +1,8 @@
+#float concept --> modulus (%) and floor division (//) Operation in python
+a = 10.2
+b= 3.5
+c = a//b #this is the floor division and will give the integer number.
+d = a%b # this will give the reminder as the answer
+
+print("c=",c)
+print("d=",d)
